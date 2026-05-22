4e0bdb789953938fb583e62331a5de8fd4dae8ee
andresalgora9-hue <andresalgora9@gmail.com>
2026-05-22 16:19:51 +0200

    Create sw.js

---
+++ public/public/sw.js
@@ -0,0 +1,17 @@
+self.addEventListener('push', function(event) {
+  const data = event.data ? event.data.json() : {};
+  const title = data.title || 'OfficioYa';
+  const options = {
+    body: data.body || 'Nueva notificación',
+    icon: '/icon-192.png',
+    badge: '/icon-192.png',
+    vibrate: [200, 100, 200],
+    data: data,
+  };
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(clients.openWindow('/'));
+});
