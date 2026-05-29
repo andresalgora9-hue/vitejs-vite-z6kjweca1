6c5690df7c9c82bf1f1e602b80cd3b9653cbb42f
andresalgora9-hue <andresalgora9@gmail.com>
2026-05-29 11:30:44 +0200

    Create constants.ts

---
+++ src/src/constants.ts
@@ -0,0 +1,6 @@
+export const PRICE_MAP: Record<string, string> = {
+  basico: "price_1TYugfCZe2kZYfZChXHjTrsg",
+  pro:    "price_1TYuioCZe2kZYfZChYFbWcrt",
+  elite:  "price_1TYuneCZe2kZYfZCxD24mHGx",
+  elite_recurrente: "price_1TYxjFCZe2kZYfZCNN2TBfzs",
+};
