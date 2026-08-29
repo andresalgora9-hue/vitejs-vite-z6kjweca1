fd8687916ac5d1c7c81527f1e410bebfcfcc69aa
andresalgora9-hue <andresalgora9@gmail.com>
2026-08-29 19:43:48 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3071,6 +3071,7 @@ setUnreadChats(Object.values(counts).reduce((a:number,b:number)=>a+b,0));
                 </div>
               ))}
             </div>
+                                           </div>
           </div>
           {loading?<Spin/>:(
             <>
